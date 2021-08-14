072d0a6b56f8ebe5d0791208e9d85820b3a57d49
DESKTOP-O1U6U1E\prutm <prutmaild@gmail.com>
2021-08-14 23:33:19 +0200

    Started working on vuilnisbelt minigame

--- YEW GAME JAM/Assets/Scripts/ItemSlot.cs
+++ YEW GAME JAM/Assets/Scripts/ItemSlot.cs
@@ -5,11 +5,13 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
+    
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag != null)
         {
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            Debug.Log(eventData.pointerDrag.name);
         }
     }
 }
